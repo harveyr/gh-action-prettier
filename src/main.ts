6afc8540a74267bd8393e37bff721e47c4d8c71c
Harvey Rogers <harveyr@gmail.com>
2019-11-13 22:06:23 -0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,78 +1,31 @@
 import * as core from '@actions/core'
+import * as kit from 'github-actions-kit'
 import * as prettier from './prettier'
 
-// TODO: Use a TS import once this is fixed: https://github.com/actions/toolkit/issues/199
-// import * as github from '@actions/github'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const github = require('@actions/github')
-
-const { GITHUB_REPOSITORY, GITHUB_SHA, GITHUB_WORKSPACE } = process.env
-
-interface Annotation {
-  annotation_level: 'notice' | 'warning' | 'failure'
-  end_column?: number
-  end_line: number
-  message: string
-  path: string
-  raw_details?: string
-  start_column?: number
-  start_line: number
-  title?: string
-}
-
 async function postCheckRun(flaggedFiles: string[]): Promise<void> {
-  if (!GITHUB_WORKSPACE) {
-    return core.setFailed(
-      'GITHUB_WORKSPACE not set. This should happen automatically.',
-    )
-  }
-  if (!GITHUB_REPOSITORY) {
-    return core.setFailed('GITHUB_REPOSITORY was not set')
-  }
-  if (!GITHUB_SHA) {
-    return core.setFailed('GITHUB_SHA was not set')
-  }
-
-  const githubToken = core.getInput('github-token')
-  if (!githubToken) {
-    return core.setFailed('github-token is required')
-  }
-  const client = new github.GitHub(githubToken)
-
-  const [owner, repo] = GITHUB_REPOSITORY.split('/')
-  core.debug(`Found Github owner ${owner}, repo ${repo}`)
-
-  const annotations: Annotation[] = []
-  for (const path of flaggedFiles) {
-    annotations.push({
-      path,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      start_line: 1,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      end_line: 1,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      annotation_level: 'failure',
-      message: 'Prettier would reformat this file',
-    })
-  }
-
-  await client.checks.create({
+  kit.postCheckRun({
+    githubToken: core.getInput('github-token'),
     name: 'Prettier',
     conclusion: flaggedFiles.length === 0 ? 'success' : 'failure',
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    head_sha: GITHUB_SHA,
-    owner,
-    repo,
-    output: {
-      title: 'Prettier',
-      summary: flaggedFiles.length ? 'Flagged files' : 'No flagged files',
-      text: flaggedFiles.join('\n'),
-      annotations,
-    },
+    summary: flaggedFiles.length ? 'Flagged files' : 'No flagged files',
+    text: flaggedFiles.join('\n'),
+    annotations: flaggedFiles.map(path => {
+      return {
+        path,
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        start_line: 1,
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        end_line: 1,
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        annotation_level: 'failure',
+        message: 'Prettier would reformat this file',
+      }
+    }),
   })
 }
 
 async function run(): Promise<void> {
+  const cwd = core.getInput('working-directory')
   const patterns = core
     .getInput('patterns')
     .split(' ')
@@ -85,11 +38,9 @@ async function run(): Promise<void> {
 
   // Cause the version to be printed to the logs. We want to make sure we're
   // using the version in the repo under test, not the one from this repo.
-  await prettier.getVersion()
+  await prettier.getVersion({ cwd })
 
-  const output = await prettier.run(patterns, {
-    cwd: core.getInput('working-directory'),
-  })
+  const output = await prettier.run(patterns, { cwd })
   const flaggedFiles: string[] = output
     .trim()
     .split('\n')
